0adc99c32f1db0f056c9ef873a19c65fa9294ae3
ttalgihon <vivithefool@gmail.com>
2018-12-04 13:48:18 -0500

    working js & css path

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -4,17 +4,19 @@ var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 // A function that randomly picks a letter from the array and stores it
 function pickingletter(alphabet) {
     var pickedletter = alphabet[Math.floor(Math.random()*alphabet.length)];
-    console.log(pickedletter); 
-};
+    console.log(pickedletter);
+    return;
+}; 
 // A player will now also pick a letter from the alphabet
 var array = [];
-// This listens to what key the player press and activates a function
-document.onkeypress = myfunction ;
+// This listens to what key the player presses and activates a function
+window.addEventListener("keypress", myfunction);
 // This function adds the letter press by the player in an empty array
 
 // This function will add the value as a string in the <p> of guesses so far
-function myfunction () {
-    var x = document.getElementById("demo").value;
-    document.getElementById("demo").innerHTML = x;
+function myfunction (x) {
+    var x = array.push();
+    document.getElementById("demo").textContent = array;
+    console.log(x);
 }
 
