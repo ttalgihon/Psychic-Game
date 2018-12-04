73c1cc931742e095490774a55785f19ed9210dfc
ttalgihon <vivithefool@gmail.com>
2018-12-04 16:23:06 -0500

    getting the js to working but also rewriting it

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,6 +1,10 @@
 // The computer will pick a letter from this array
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 "q","r","s","t","u","v","w","x","y","z"];
+var win = 0;
+var lose = 0;
+var numGuessleft = 9;
+var playerChoice = [];
 // A function that randomly picks a letter from the array and stores it
 function pickingletter(alphabet) {
     var pickedletter = alphabet[Math.floor(Math.random()*alphabet.length)];
@@ -8,15 +12,47 @@ function pickingletter(alphabet) {
     return;
 }; 
 // A player will now also pick a letter from the alphabet
-var array = [];
-// This listens to what key the player presses and activates a function
-window.addEventListener("keypress", myfunction);
-// This function adds the letter press by the player in an empty array
+let keyPressed = alphabet;
+// doc.onkeyup listens to what key the player presses and activates a function
+// The function adds the letter press by the player in an empty array.
+document.onkeyup = function (event) {
+    keyPressed = event.key.toLowerCase();
+    playerChoice.push(keyPressed); 
+    document.getElementById("demo").textContent = playerChoice;
+    console.log(playerChoice);
+    return
+}
+//keypress holds the letter.
+// playerchoice holds all the keys pressed by the player.
 
-// This function will add the value as a string in the <p> of guesses so far
-function myfunction (x) {
-    var x = array.push();
-    document.getElementById("demo").textContent = array;
-    console.log(x);
+// if the pickedletter by the computer equals the playerchoice letter that was typed...
+// it will add +1 to the win variable.
+var isDuplicate = false;
+for (var i = 0; i<=9; i++) {
+    if ( pickedletter == playerChoice[i]) {
+        isDuplicate = true;
+        win++;
+        break;
+    }
 }
 
+document.onkeypress = function(event) {
+    keyPressed =event.key.toLocaleLowerCase;
+    var pickedletter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    if (alphabet.indexOf(keyPressed) > -1) {
+        if (keyPressed === pickedletter) {
+            wins++;
+            numGuessleft = 9;
+            playerChoice [];
+        }
+        if ( keyPressed != pickingletter) {
+            numGuessleft --;
+            playerChoice [];
+        }
+    }
+};
+
+document.getElementById("winP").textContent = win;
+document.getElementById("loseP").textContent = lose;
+document.getElementById("guessP").textContent = numGuessleft;
+document.getElementById("demo").textContent = playerChoice;
