cf656c0d076308705fe7d332c86e4f775a134056
ttalgihon <vivithefool@gmail.com>
2018-12-08 12:07:27 -0500

    Worked on it some more.

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -5,8 +5,23 @@ for (let i = 97; i <= 122; i++) {
     // A variable that randomly picks a letter from var 'alphabet' for the computer.  
 };
 var pickedcomputerletter = alphabet[Math.floor(Math.random()*alphabet.length)];
-console.log(alphabet);
-console.log(pickedcomputerletter);
+
+let pickedPlayerLetter;
+document.onkeyup = function storingpressedkey(letter) {
+    pickedPlayerLetter = letter.key.toLowerCase();
+    if (pickedcomputerletter == pickedPlayerLetter && gamestats.numGuessleft != 0) {
+        gamestats.win++;
+        gamestats['playerChoice'] = pickedPlayerLetter;
+        console.log(pickedPlayerLetter);
+    } 
+    else {
+        gamestats.lose++;
+        gamestats.numGuessleft--;
+        gamestats['playerChoice']= pickedPlayerLetter;  
+        console.log(pickedPlayerLetter);  
+    };
+    
+};
 
 // Create an object
 var gamestats = {
@@ -15,36 +30,14 @@ var gamestats = {
     numGuessleft: 9,
     playerChoice: [""],
 };
+
+log = console.log;
+log(alphabet);
+log(pickedcomputerletter);
+log(gamestats);
+
 //Display the object gamestats data in the html.
-console.log(gamestats);
-console.log(gamestats.win); 
-document.getElementById("winP").innerHTML = gamestats.win;
+document.getElementById("winP").textContent = gamestats.win;
 document.getElementById("loseP").textContent = gamestats.lose;
 document.getElementById("guessP").textContent = gamestats.numGuessleft;
 document.getElementById("demo").textContent = gamestats.playerChoice;
-
-// A variable that holds the letter pressed by the player in lower case then,
-// pushed that letter into an array called playerchoices.
-
-// let pickedplayerletter = document.onkeypress.toLowerCase();
-// gamestats.playerChoice.push(pickedplayerletter);
-// console.log(pickedplayerletter);
-
-let pickedPlayerLetter;
-document.onkeyup = function storingpressedkey(letter) {
-    pickedPlayerLetter = letter.key.toLowerCase();
-    if (pickedcomputerletter === pickedPlayerLetter && gamestats.numGuessleft != 0) {
-        gamestats.win++;
-        
-    } else {
-        gamestats.lose++;
-        gamestats.numGuessleft--;
-        gamestats['playerChoice']= pickedPlayerLetter;    
-    };
-    
-};
-
-// if pickedplayer letter is = to computerpicked letter
-//THEN it will change win by ++
-// if they do NOT = 
-//THEN take away from guessesleft -- by one and put the playerpicked letter in the playerchoice array. 
